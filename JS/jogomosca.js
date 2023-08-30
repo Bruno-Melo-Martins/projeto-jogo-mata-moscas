41466d5aa88b2473587cb5ea6b0aaeb4f8b432aa
Bruno-Melo-Martins <brumelo029@hotmail.com>
2023-08-29 21:37:00 -0300

    29 de agosto

--- JS/jogomosca.js
+++ JS/jogomosca.js
@@ -1,28 +1,28 @@
 function SelecaoDificuldade(){
     dificuldade = (document.getElementById('dificuldades').value);
     if(dificuldade == '1'){
-        tempoJogo = 20
-        tempoMosca = 5
-        moscas = 4
+        sessionStorage.setItem('tempoJogo','20')
+        sessionStorage.setItem('tempoMosca','5')
+        sessionStorage.setItem('moscas','5')
     }
     if(dificuldade == '2'){
-        tempoJogo = 18
-        tempoMosca = 3
-        moscas = 6
+        sessionStorage.setItem('tempoJogo','18')
+        sessionStorage.setItem('tempoMosca','3')
+        sessionStorage.setItem('moscas','6')
     }
     if(dificuldade == '3'){
-        tempoJogo = 16
-        tempoMosca = 2
-        moscas = 8
+        sessionStorage.setItem('tempoJogo','16')
+        sessionStorage.setItem('tempoMosca','2')
+        sessionStorage.setItem('moscas','8')
     }
     if(dificuldade == '4'){
-        tempoJogo = 15
-        tempoMosca = 1.5
-        moscas = 10
+        sessionStorage.setItem('tempoJogo','15')
+        sessionStorage.setItem('tempoMosca','1')
+        sessionStorage.setItem('moscas','10')
     }
 }
 
-function IniciarJogo(){
+function Redirecionar(){
     if(dificuldade == 0){
         document.getElementById('aviso').innerHTML='Insira uma dificuldade válida';
         segundo = 5;
@@ -34,7 +34,68 @@ function IniciarJogo(){
             }
         },1000)
     }else{
-        window.location.href='../jogomosca.html'
+        window.location.href='../jogomosca.html';
         
     }
+}
+
+var altura=0;
+var largura=0;
+function ajustaTamanhoPalco(){
+    altura = window.innerHeight;
+    largura = window.innerWidth;
+    console.log(altura, largura);
+}
+
+ajustaTamanhoPalco()
+console.log(altura, largura);
+
+function posicaoRandomica(){
+    var posicaoX=Math.floor(Math.random()*(largura-100));
+    var posicaoY=Math.floor(Math.random()*(altura-100));
+
+    /*criar o nosso mosquito (
+        elemento HTML) no DOM*/
+        var mosquito=document.createElement('img');
+        mosquito.src='../imagens/mosquito.png';
+        document.body.appendChild(mosquito);
+        mosquito.setAttribute('id','mosquito1');
+        document.getElementById('mosquito1').style.position='absolute'
+        document.getElementById('mosquito1').style.left=posicaoX+'px'
+        document.getElementById('mosquito1').style.top=posicaoY+'px'
+        mosquito.setAttribute('onclick','numeroMosca()')
+}
+
+function numeroMosca(){
+    if(mosquitos = ''){
+        mosquitos = sessionStorage.getItem('moscas')
+    }
+    mosquitos--;
+    document.getElementById('mosquito1').remove();
+    if(mosquitos == 0){
+        window.location.href='../vitoria.html'
+    }
+}
+
+var checagem = 50
+function iniciarJogo(){
+    let tempoJogo = sessionStorage.getItem('tempoJogo')
+    let tempoMosca = sessionStorage.getItem('tempoMosca')
+    relogio = setInterval(function(){
+        document.getElementById('contador').innerHTML=tempoJogo
+        tempoJogo--;
+        if(checagem == 50){
+            posicaoRandomica()
+            checagem = 0
+        }
+        checagem++;
+        if(checagem == tempoMosca){
+            document.getElementById('mosquito1').remove();
+            posicaoRandomica()
+            checagem = 0
+        }
+        if(tempoJogo == 0 || tempoJogo < 0){
+            window.location.href='../gameover.html'
+        }
+    },1000)
 }
--- js/jogo.js
+++ js/jogo.js
@@ -11,18 +11,33 @@ function ajustaTamanhoPalco(){
 ajustaTamanhoPalco()
 console.log(altura, largura);
 
+moscas = 5
 //vamos criar uma posição 
 //randomica para o mosquito
 function posicaoRandomica(){
-    var posicaoX=Math.floor(Math.random()*largura);
-    var posicaoY=Math.floor(Math.random()*altura);
+    var posicaoX=Math.floor(Math.random()*(largura-100));
+    var posicaoY=Math.floor(Math.random()*(altura-100));
     console.log(posicaoX,posicaoY)
 
     /*criar o nosso mosquito (
         elemento HTML) no DOM*/
-        var mosquito=document.createElement('img')
+        var mosquito=document.createElement('img');
         mosquito.src='../imagens/mosquito.png';
         document.body.appendChild(mosquito);
-        mosquito.setAttribute('class','mosquito1')
+        mosquito.setAttribute('id','mosquito1');
+        document.getElementById('mosquito1').style.position='absolute'
+        document.getElementById('mosquito1').style.left=posicaoX+'px'
+        document.getElementById('mosquito1').style.top=posicaoY+'px'
+        mosquito.setAttribute('onclick','checagem()');
     }
 posicaoRandomica()
+
+function checagem(){
+    if(moscas != 1){
+    document.getElementById('mosquito1').remove();
+    moscas--;
+    posicaoRandomica()
+    }else{
+        window.location.href='../vitoria.html'
+    }
+}
