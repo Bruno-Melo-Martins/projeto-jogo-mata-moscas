2db85af060db30fa31c06bdd7d3e1b363f3d13c9
Bruno-Melo-Martins <133404191+Bruno-Melo-Martins@users.noreply.github.com>
2023-08-30 13:26:12 -0300

    t

--- js/jogo.js
+++ js/jogo.js
@@ -13,9 +13,10 @@ console.log(altura, largura);
 
 //vamos criar uma posição 
 //randomica para o mosquito
+quantidade = 5
 function posicaoRandomica(){
-    var posicaoX=Math.floor(Math.random()*largura);
-    var posicaoY=Math.floor(Math.random()*altura);
+    var posicaoX=Math.floor(Math.random()*(largura - 115));
+    var posicaoY=Math.floor(Math.random()*(altura - 115));
     console.log(posicaoX,posicaoY)
 
     /*criar o nosso mosquito (
@@ -23,6 +24,18 @@ function posicaoRandomica(){
         var mosquito=document.createElement('img')
         mosquito.src='../imagens/mosquito.png';
         document.body.appendChild(mosquito);
-        mosquito.setAttribute('class','mosquito1')
+        mosquito.setAttribute('id','mosquito1')
+        mosquito.style.position='relative'
+        mosquito.style.top=posicaoY+'px'
+        mosquito.style.left=posicaoX+'px'
+        mosquito.setAttribute('onclick','remover()')
     }
-posicaoRandomica()
+function remover(){
+    if(quantidade != 0){
+        quantidade--
+    elemento=document.getElementById('mosquito1')
+    elemento.remove()
+    posicaoRandomica()}else{
+        window.location="../vitoria.html"
+    }
+}
